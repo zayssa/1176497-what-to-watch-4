2830c8a2099ea8efd87ba21e165dbebba8f1f247
Marina Petrova <zayssa@mail.ru>
2020-06-09 21:32:39 +0300

    1-3

---
+++ src/components/App/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Main from "../Main/index.jsx";
+
+
+const App = (data) => {
+  return (
+    <Main {...data} />
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App/index.jsx";
 
+const data = {
+  name: `atata`,
+  genre: `bugaga`,
+  date: `2026`,
+};
+
+ReactDOM.render(
+    <App {...data} />,
+    document.querySelector(`#root`)
+);
