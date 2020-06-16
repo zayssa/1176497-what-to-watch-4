c09a65df9c25610f739fc39fb6c693e6752760a1
Marina Petrova <zayssa@mail.ru>
2020-06-16 19:33:23 +0300

    fix 2-2

--- src/index.js
+++ src/index.js
@@ -3,30 +3,30 @@ import ReactDOM from "react-dom";
 import App from "./components/app/app.jsx";
 
 const data = {
-  name: `atata`,
-  genre: `bugaga`,
-  date: `2026`,
+  name: `Pila`,
+  genre: `comedy`,
+  date: `2222`,
   films: [
-    `Fantastic Beasts: The Crimes of Grindelwald`,
-    `Bohemian Rhapsody`,
-    `Macbeth`,
-    `Aviator`,
-    `We need to talk about Kevin`,
-    `What We Do in the Shadows`,
-    `Revenant`,
-    `Johnny English`,
-    `Shutter Island`,
-    `Pulp Fiction`,
-    `No Country for Old Men`,
-    `Snatch`,
-    `Moonrise Kingdom`,
-    `Seven Years in Tibet`,
-    `Midnight Special`,
-    `War of the Worlds`,
-    `Dardjeeling Limited`,
-    `Orlando`,
-    `Mindhunter`,
-    `Midnight Specialist`
+    `Titanic`,
+    `Harry Poter`,
+    `Forest Gamp`,
+    `Lost`,
+    `1+1`,
+    `Hobbit`,
+    `Hatchi`,
+    `Home alone`,
+    `Angel`,
+    `Fury`,
+    `Hancock`,
+    `The ABC murders`,
+    `Hangover`,
+    `The passage`,
+    `The boyfreind`,
+    `Limitless`,
+    `Ice age`,
+    `The silence`,
+    `Sahara`,
+    `Homefront`
   ]
 };
 
